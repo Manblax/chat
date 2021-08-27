df48543a0a90742da418011ab2269346c07b66ff
manblax <manblax@yandex.ru>
2021-08-27 18:05:48 +0300

    Добавить тесты

--- api/chat/tests.py
+++ api/chat/tests.py
@@ -3,6 +3,8 @@ from channels.testing import ChannelsLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.wait import WebDriverWait
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
 
 
 class ChatTests(ChannelsLiveServerTestCase):
@@ -13,7 +15,7 @@ class ChatTests(ChannelsLiveServerTestCase):
         super().setUpClass()
         try:
             # NOTE: Requires "chromedriver" binary to be installed in $PATH
-            cls.driver = webdriver.Chrome()
+            cls.driver = webdriver.Remote("http://chrome:4444/wd/hub", options=options)
         except:
             super().tearDownClass()
             raise
--- api/mysite/settings.py
+++ api/mysite/settings.py
@@ -125,7 +125,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('172.20.0.1', 6379)],
+            "hosts": [('reddis', 6379)],
         },
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
