c3244ef6030fc5567292dd1139a9ddf4d89846d4
manblax <manblax@yandex.ru>
2021-08-27 18:58:20 +0300

    postgres

--- api/mysite/settings.py
+++ api/mysite/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_POST'),
     }
 }
 
@@ -127,10 +131,10 @@ CHANNEL_LAYERS = {
         'CONFIG': {
             "hosts": [('reddis', 6379)],
         },
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.path.join(BASE_DIR, 'db.postgresql'),
         'TEST': {
-            'NAME': os.path.join(BASE_DIR, 'db_test.sqlite3')
+            'NAME': os.path.join(BASE_DIR, 'db_test.postgresql')
         }
     },
 }
